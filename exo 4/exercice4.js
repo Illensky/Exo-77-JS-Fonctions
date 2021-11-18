b557affde2233f1920e2a9702fd20877280e0053
Illensky <alexis.laroche.02240@gmail.com>
2021-11-18 13:35:42 +0100

    Exercice done

--- exo 4/exercice4.js
+++ exo 4/exercice4.js
@@ -1,10 +1,12 @@
 /* Ecrire ci dessous une fonction permettant d'ajouter du texte dans le div appelé container */
 
-
+function textAdd () {
+    document.getElementById('container').innerHTML = "ajout de texte"
+}
 
 /* Appeler la fonction */
 
-
+textAdd()
 
 
 
